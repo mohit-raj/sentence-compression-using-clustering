44da861604c3d358a2e1ae94dd03a276c658ece8
mohit raj <mohit.raj@st.niituniversity.in>
2017-07-03 17:54:46 +0530

    Created a revision for term document matrix

--- core.py/main.py
+++ core.py/main.py
@@ -6,7 +6,7 @@ class article (object):
 
     s_id = 0
     s_length = 0
-    s_normalised_length = 0.0
+    s_string =""
 
     def __init__(self, index):
         self.index = index
@@ -17,67 +17,37 @@ if __name__ == '__main__':
     noOfSentences = 0
     summation = 0.0
 
-    with open('sample-article.txt') as file:
+    with open('/home/mohit/Desktop/sentence-compression-using-clustering/core.py/Input documents CORPUS/sample-article-1.txt') as file:
         for eachLine in file:
             summation += len(eachLine.split()) ** 2
             noOfSentences += 1
     
     summation = math.sqrt(summation)
     obj = []
+    doc = []
 
     for i in range(noOfSentences):
         obj.append(article(i))
 
     index = 0
     
-    with open('sample-article.txt') as file:
+    with open('/home/mohit/Desktop/sentence-compression-using-clustering/core.py/Input documents CORPUS/sample-article-1.txt') as file:
         for eachLine in file:
             
             obj[index].s_id = index
+            obj[index].s_string = eachLine
             obj[index].s_length = len(eachLine.split())
-            obj[index].s_normalised_length = obj[index].s_length / summation
             index += 1
-     
+    print noOfSentences
+
     for i in range(noOfSentences):
-        print obj[i].s_length
-        print obj[i].s_normalised_length
-        print '.'
+        doc.append(obj[i].s_string)
     
-    # obj = sorted(obj, key = lambda article: article.s_length) 
+    matrix = textmining.TermDocumentMatrix();
+    
+    for i in doc:
+        matrix.add_doc(i)
 
-    pointFirst = min(obj,key = attrgetter('s_length'))
-    pointSecond = max(obj, key = attrgetter('s_length'))
-
-    # print pointFirst.s_normalised_length
-    # print pointSecond.s_normalised_length
-
-
-    # # for i in range(noOfSentences):
-    #     print obj[i].s_id
-    # pointFirst = min_num
-    # pointSecond = obj[len(obj) - 1]
-
-    # print noOfSentences
-
-    cluster = [[] for n in range(noOfSentences)]
-    cluster[0].append(pointFirst)
-    cluster[1].append(pointSecond)
-
-    # for i in range(1, (len(obj) - 1)):
-
-    #     distSimilarityFirst = pointFirst.s_normalised_length * obj[i].s_normalised_length
-    #     distSimilaritySecond = pointSecond.s_normalised_length * obj[i].s_normalised_length
-
-    #     if(distSimilarityFirst <= distSimilaritySecond):
-    #         # print distSimilarityFirst
-    #         # print distSimilaritySecond
-    #         # print '.'
-    #         print "kk"
-    #         cluster[0].append(obj[i])
-
-    #     elif(distSimilaritySecond <= distSimilarityFirst):
-    #         # print "mohit"
-    #         cluster[1].append(obj[i])
-
-    # for i in cluster[1]:
-    #     print i.s_id
+    matrix.write_csv('/home/mohit/Desktop/sentence-compression-using-clustering/core.py/Matrix Generated Output/matrix.csv', cutoff=1)
+    for row in matrix.rows(cutoff=1):
+            print row
