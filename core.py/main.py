e344abd9f12c2ae036e40e6d1d620a78c13bd212
mohit raj <mohit.raj@st.niituniversity.in>
2017-07-05 21:38:11 +0530

    Modified main.py

--- core.py/main.py
+++ core.py/main.py
@@ -1,4 +1,5 @@
 import math
+import csv
 import textmining
 from operator import itemgetter, attrgetter
 
@@ -6,6 +7,7 @@ class article (object):
 
     s_id = 0
     s_length = 0
+    s_normalised_lenght = 0
     s_string =""
 
     def __init__(self, index):
@@ -15,14 +17,11 @@ class article (object):
 if __name__ == '__main__':
 
     noOfSentences = 0
-    summation = 0.0
 
-    with open('/home/mohit/Desktop/sentence-compression-using-clustering/core.py/Input documents CORPUS/sample-article-1.txt') as file:
+    with open('/home/mohit/Desktop/sentence-compression-using-clustering/Input documents CORPUS/sample-article-1.txt') as file:
         for eachLine in file:
-            summation += len(eachLine.split()) ** 2
             noOfSentences += 1
     
-    summation = math.sqrt(summation)
     obj = []
     doc = []
 
@@ -31,14 +30,14 @@ if __name__ == '__main__':
 
     index = 0
     
-    with open('/home/mohit/Desktop/sentence-compression-using-clustering/core.py/Input documents CORPUS/sample-article-1.txt') as file:
+    with open('/home/mohit/Desktop/sentence-compression-using-clustering/Input documents CORPUS/sample-article-1.txt') as file:
         for eachLine in file:
             
             obj[index].s_id = index
             obj[index].s_string = eachLine
             obj[index].s_length = len(eachLine.split())
+            obj[index].s_normalised_lenght = 0
             index += 1
-    # print noOfSentences
 
     for i in range(noOfSentences):
         doc.append(obj[i].s_string)
@@ -48,18 +47,20 @@ if __name__ == '__main__':
     for i in doc:
         matrix.add_doc(i)
 
-    matrix.write_csv('/home/mohit/Desktop/sentence-compression-using-clustering/core.py/Matrix Generated Output/matrix.csv', cutoff=1)
-    # for row in matrix.rows(cutoff=1):
-    #     print row
+    matrix.write_csv('/home/mohit/Desktop/sentence-compression-using-clustering/Matrix Generated Output/matrix.csv', cutoff=1)
 
-    # for column in matrix.columns(cutoff=1):
-    #     print column
+    data = list(csv.reader(open('/home/mohit/Desktop/sentence-compression-using-clustering/Matrix Generated Output/matrix.csv')))
+  
+    index = 0
+    for rows in data[1:]:
+        for item in rows:
+            obj[index].s_normalised_lenght = obj[index].s_normalised_lenght + (int(item) ** 2)
 
-    count = 0
-    with open('/home/mohit/Desktop/sentence-compression-using-clustering/core.py/Matrix Generated Output/matrix.csv') as file:
-        for eachLine in file:
-            count += 1
-            if(count == 1):
-                continue
-            else:
-                
+        obj[index].s_normalised_lenght = math.sqrt(obj[index].s_normalised_lenght)
+        index += 1
+               
+    print obj[0].s_normalised_lenght
+    print obj[1].s_normalised_lenght
+    print obj[2].s_normalised_lenght
+    print obj[3].s_normalised_lenght
+    
